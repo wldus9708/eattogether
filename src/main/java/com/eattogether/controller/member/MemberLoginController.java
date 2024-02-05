4b7047bb5351a42e9921883e16e08ee08379fdce
honey6352 <105294300+honey6352@users.noreply.github.com>
2024-02-05 20:48:03 +0900

    로그인 컨트롤러, 멤버 다오 진행중

--- src/main/java/com/eattogether/controller/member/MemberLoginController.java
+++ src/main/java/com/eattogether/controller/member/MemberLoginController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.eattogether.common.Superclass;
+import com.eattogether.model.bean.Member;
+import com.eattogether.model.dao.MemberDao;
 
 public class MemberLoginController extends Superclass {
 	private final String PREFIX = "member/";
@@ -13,6 +15,33 @@ public class MemberLoginController extends Superclass {
 		System.out.println("로그인 페이지로 이동합니다.");
 		super.doGet(request, response);
 		super.gotoPage(PREFIX + "login.jsp");
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		super.doPost(request, response);
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		System.out.println(id + "/" + password);
+
+		MemberDao dao = new MemberDao();
+		Member bean = dao.getDataByIdAndPassword(id, password);
+		System.out.println(bean);
+
+		if (bean == null) {
+			System.out.println( "로그인 정보가 잘못되었습니다.");
+			String message = "로그인 정보가 잘못되었습니다.";
+			super.setAlertMessage(message);
+			super.gotoPage(PREFIX + "login.jsp");
+		} else { // 로그인 성공
+			// session 영역(scope)에 나의 로그인 정보를 저장(바인딩)합니다.
+			// loginfo 속성을 사용하여 현재 로그인 상태를 확인할 수 있습니다.
+			super.session.setAttribute("loginfo", bean);
+			
+			// 로그인 성공 이후 메인 페이지로 이동합니다.
+			super.gotoPage("common/main.jsp");
+		}
 	}
 
 }
--- src/main/java/com/eattogether/model/dao/MemberDao.java
+++ src/main/java/com/eattogether/model/dao/MemberDao.java
@@ -1,10 +1,53 @@
 package com.eattogether.model.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.eattogether.model.bean.Member;
 
 public class MemberDao extends SuperDao {
+	
+	public Member getDataByIdAndPassword(String id, String password) {
+
+		// 아이디와 비밀번호를 이용하여 해당 회원이 존재하는지 확인합니다.
+		String sql = "select * from members where mem_id = ? and mem_password = ?";
+		PreparedStatement pstmt = null; // 문장 객체
+		ResultSet rs = null;
+		Member bean = null;
+
+		super.conn = super.getConnection();
+		try {
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, id);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+
+			// 요소들 읽어서 컬렉션에 담습니다.
+			if (rs.next()) {
+				bean = this.resultSet2Bean(rs);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				super.closeConnection();
+
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+		return bean;
+	}
 
 	public int insertData(Member bean) { // 회원 가입 insert
 		System.out.println(bean);
@@ -89,5 +132,32 @@ public class MemberDao extends SuperDao {
 		}
 
 		return cnt;
+	}
+
+	
+	
+	private Member resultSet2Bean(ResultSet rs) {
+		try {
+			
+			Member bean = new Member();
+			bean.setNo(Integer.parseInt(rs.getString("mem_no")));
+			bean.setId(rs.getString("mem_id"));
+			bean.setName(rs.getString("mem_name"));
+			bean.setAlias(rs.getString("mem_alias"));
+			bean.setPassword(rs.getString("mem_password"));
+			bean.setSocial_key(rs.getString("mem_social_key"));
+			bean.setSocial_host(rs.getString("mem_social_host"));
+			bean.setBirth(String.valueOf(rs.getDate("mem_birth")));
+			bean.setPhone(rs.getString("mem_phone"));
+			bean.setTaste(rs.getString("mem_taste"));
+			bean.setPicture(rs.getString("mem_picture"));
+			bean.setFlag(rs.getString("mem_flag"));
+
+			return bean;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }

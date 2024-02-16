0bc246ff670d36b44ee5375087aa01d68814a51e
phwooName <phwoo@example.com>
2024-02-16 17:20:32 +0900

    레시피 수정 커밋
    레시피 수정 커밋

--- src/main/java/com/eattogether/controller/recipe/RecipeDetailController.java
+++ src/main/java/com/eattogether/controller/recipe/RecipeDetailController.java
@@ -11,7 +11,6 @@ import com.eattogether.common.Superclass;
 import com.eattogether.model.bean.Member;
 import com.eattogether.model.bean.Recipe;
 import com.eattogether.model.bean.Star;
-import com.eattogether.model.bean.combo01;
 import com.eattogether.model.dao.RecipeDao;
 
 public class RecipeDetailController extends Superclass {
@@ -25,20 +24,12 @@ public class RecipeDetailController extends Superclass {
 
 		int rec_no = Integer.parseInt(request.getParameter("rec_no"));
 		Recipe bean = dao.getDataBean(rec_no);
-	
-		Star mbean =dao.getDataBean0(rec_no);
-		combo01 combo=dao.getDateBean3(rec_no);
-		
-		request.setAttribute("combo", combo);
-		System.out.println(mbean);
-		if(mbean==null) {
-			request.setAttribute("a", 0);
-		}else {
-			if (rec_no == mbean.getRec_no()) {
-				request.setAttribute("a", 1);
-			}
+
+		Star mbean = dao.getDataBean0(rec_no);
+		if (mbean != null && rec_no == mbean.getRec_no()) { // mbean이 null이 아니고, rec_no와 mbean의 rec_no가 같은 경우에만 실행
+			request.setAttribute("a", 1);
 		}
-		
+
 		if (bean == null) {
 			super.setAlertMessage("로그인을 하셔야 합니다.");
 			super.gotoPage(PREFIX + "recipedetailList.jsp"); // 페이지 확인
@@ -55,7 +46,7 @@ public class RecipeDetailController extends Superclass {
 			}
 
 		}
-		//최근본 목록
+		// 최근본 목록
 		HttpSession session = request.getSession();
 		Integer nV = (Integer) session.getAttribute("sk");
 		if (nV == null) {
@@ -88,18 +79,13 @@ public class RecipeDetailController extends Superclass {
 		List<Recipe> testlist = new ArrayList<Recipe>();
 		for (int i = 0; i < nn; i++) {
 			if (session.getAttribute("rec_no" + i) != null) {
-				Recipe rbean= dao.getDataBean((Integer) session.getAttribute("rec_no" + i));
+				Recipe rbean = dao.getDataBean((Integer) session.getAttribute("rec_no" + i));
 				testlist.add(rbean);
 			}
 		}
 		System.out.println(testlist);
 		session.setAttribute("testList", testlist);
 
-
-
-
-		
-		
 		// 텍스트 내용을 리스트로 가져옵니다.
 		List<String> recContents = bean.getRec_contents();
 

84ae8b753c821df21cdda333c16ed3db6b4ca35d
honey6352 <105294300+honey6352@users.noreply.github.com>
2024-02-22 22:20:48 +0900

    레시피 페이지 닉네임키워드 null 체크 처리 추가 및 없는 키워드 일시 경고 안내문 추가

--- src/main/java/com/eattogether/controller/recipe/RecipeListController.java
+++ src/main/java/com/eattogether/controller/recipe/RecipeListController.java
@@ -14,94 +14,98 @@ import com.eattogether.utility.Paging;
 
 
 public class RecipeListController extends Superclass{
-private final String PREFIX = "board/";
-	
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		super.doGet(request, response);
-		System.out.println("상품 목록 페이지로 이동합니다.");
-		
-		//페이징 처리를 위한 파라미터 목록들
-		String pageNumber = request.getParameter("pageNumber");
-		String pageSize = request.getParameter("pageSize");
-		String mode = request.getParameter("mode");
-		String keyword = request.getParameter("keyword");
-		String p = request.getParameter("p");
-		
-		if ("cat_no".equals(mode)) {
-		    if ("양식".equals(keyword)) {
-		        keyword = "1";
-		    }
-		    if ("한식".equals(keyword)) {
-		        keyword = "2";
-		    }
-		    if ("중식".equals(keyword)) {
-		        keyword = "3";
-		    }
-		    if ("일식".equals(keyword)) {
-		        keyword = "4";
-		    }
-		}
-		
-		if("rec_header".equals(mode)){
-			if ("오코노미야끼".equals(keyword) || "오코노미야키".equals(keyword)|| "오꼬노미야끼".equals(keyword)) {
-		        keyword = "노미야";
-		    }
-		}
-		
-		System.out.println(keyword);
-		RecipeDao dao = new RecipeDao();
-		MemberDao mdao = new MemberDao();
+    private final String PREFIX = "board/";
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        super.doGet(request, response);
+        System.out.println("상품 목록 페이지로 이동합니다.");
+        
+        //페이징 처리를 위한 파라미터 목록들
+        String pageNumber = request.getParameter("pageNumber");
+        String pageSize = request.getParameter("pageSize");
+        String mode = request.getParameter("mode");
+        String keyword = request.getParameter("keyword");
+        String p = request.getParameter("p");
+        
+        if ("cat_no".equals(mode)) {
+            if ("양식".equals(keyword)) {
+                keyword = "1";
+            }
+            if ("한식".equals(keyword)) {
+                keyword = "2";
+            }
+            if ("중식".equals(keyword)) {
+                keyword = "3";
+            }
+            if ("일식".equals(keyword)) {
+                keyword = "4";
+            }
+        }
+        
+        if("rec_header".equals(mode)){
+            if ("오코노미야끼".equals(keyword) || "오코노미야키".equals(keyword)|| "오꼬노미야끼".equals(keyword)) {
+                keyword = "노미야";
+            }
+        }
+        
+        System.out.println(keyword);
+        RecipeDao dao = new RecipeDao();
+        MemberDao mdao = new MemberDao();
 
-		request.setAttribute("ww", 0);
-		if("mem_id".equals(mode)) {
-			Member aa=mdao.getData5(keyword);
-			keyword=aa.getId(); 
-			request.setAttribute("aa",aa);
-			request.setAttribute("ww", 1);
-		}else {
-			Member aa=mdao.getData5(keyword);
-			System.out.println("aa : "+aa);
-			if(aa != null) {
-				System.out.println("aa : "+aa.getId());
-				keyword=aa.getId(); 
-				request.setAttribute("aa",aa);
-				request.setAttribute("ww", 1);
-			}
-		}
-		int totalCount = dao.getTotalRecordCount("recipe", mode, keyword);
-		String url = super.getUrlInformation("reList");
-		boolean isGrid = true;//상품이라면 true가 됩니다.
-		Paging paging = new Paging(pageNumber, pageSize, totalCount, url, mode, keyword, isGrid, p);
-		
-		//int beginRow = 1;
-		//int endRow = 10;
-		
-		
-		
-		
-		//System.out.println("recipe 데이터목록개수 :" + dataList.size());
-		
-		request.setAttribute("paging", paging);//페이징 객체도 바인딩
-		System.out.println("p의 값: " + p);
-		
-		if("s".equals(p)) {
-			List<combo01> dataList = dao.getDataList(paging);
-			request.setAttribute("pano", pageNumber);
-			request.setAttribute("dataList", dataList);
-			
-		}else if("o".equals(p)){
-
-			List<combo01> dataList2 = dao.getDataList2(paging);
-			request.setAttribute("pano", pageNumber);
-			request.setAttribute("dataList", dataList2);
-		}else {
-
-			List<combo01> dataList3 = dao.getDataList3(paging);
-			request.setAttribute("pano", pageNumber);
-			request.setAttribute("dataList", dataList3);
-		}
-		
-		super.gotoPage(PREFIX + "recipe.jsp");
-	}
+        request.setAttribute("ww", 0);
+        if (keyword != null && !keyword.trim().isEmpty()) { // 입력된 키워드가 있는 경우
+            if("mem_id".equals(mode)) {
+                Member aa = mdao.getData5(keyword);
+                if (aa != null) {
+                    System.out.println("aa : " + aa.getId());
+                    keyword = aa.getId(); 
+                    request.setAttribute("aa", aa);
+                    request.setAttribute("ww", 1);
+                } else {
+                    // 키워드에 해당하는 멤버가 없는 경우 경고 메시지 설정
+                    super.setAlertMessage("존재하지 않는 닉네임입니다.");
+                }
+            } else {
+                Member aa = mdao.getData5(keyword);
+                System.out.println("aa : " + aa);
+                if (aa != null) {
+                    System.out.println("aa : " + aa.getId());
+                    keyword = aa.getId(); 
+                    request.setAttribute("aa", aa);
+                    request.setAttribute("ww", 1);
+                } else {
+                    // 키워드에 해당하는 정보가 없는 경우 경고 메시지 설정
+                    super.setAlertMessage("존재하지 않는 키워드입니다.");
+                }
+            }
+        } else {
+        	// 키워드 없이 빈 공백으로 검색할시
+        	
+        }
+        
+        int totalCount = dao.getTotalRecordCount("recipe", mode, keyword);
+        String url = super.getUrlInformation("reList");
+        boolean isGrid = true;//상품이라면 true가 됩니다.
+        Paging paging = new Paging(pageNumber, pageSize, totalCount, url, mode, keyword, isGrid, p);
+        
+        request.setAttribute("paging", paging); // 페이징 객체 바인딩
+        System.out.println("p의 값: " + p);
+        
+        if("s".equals(p)) {
+            List<combo01> dataList = dao.getDataList(paging);
+            request.setAttribute("pano", pageNumber);
+            request.setAttribute("dataList", dataList);
+        } else if("o".equals(p)) {
+            List<combo01> dataList2 = dao.getDataList2(paging);
+            request.setAttribute("pano", pageNumber);
+            request.setAttribute("dataList", dataList2);
+        } else {
+            List<combo01> dataList3 = dao.getDataList3(paging);
+            request.setAttribute("pano", pageNumber);
+            request.setAttribute("dataList", dataList3);
+        }
+        
+        super.gotoPage(PREFIX + "recipe.jsp");
+    }
 }
